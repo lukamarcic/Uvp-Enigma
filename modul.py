2181206ae6447a7a495735eceff99c79d9fbd7bc
Luka Marčič <marcicluke@gmail.com>
2022-06-27 11:48:36 +0200

    Ureditev funkcij v modulu in opisov funkcij

--- modul.py
+++ modul.py
@@ -1,34 +1,26 @@
 import string
 import random
 
-# Najprej sestavimo funkcijo, ki besedilo spremeni v sprejemljivo obliko
-# To pomeni da odstrani vse znake, ki niso v angleški abecedi in spremeni vse črke v male
-# Presledke pusti pri miru, če nastane več zaporednih presledkov jih spremeni v enega
-# Šumnike spremeni v črke brez strešice
-# Za angleško abecedo importamo string
 
-def uredi_besedilo(tekst):
-    delno_urejen1 = tekst.lower().replace('č', 'c').replace('š', 's').replace('ž', 'z')
-    delno_urejen2 = ''
-    ang_abc = list(string.ascii_lowercase)
+# Enigmo sestavljajo trije ključni deli: plugboard, rotorji in "zrcalo"
+# Mehanično je delovala tako, da se je ob pritisku na tipko sklenil električni tok
+# Ta je potoval skozi posamezne dele enigme, kjer se je večkrat spremenil v drugo črko
+# Programsko bomo te posamezne dele enigme predstavili kot permutacije črk oz. številk, ki črke predstavljajo
+# Prvi del modula zajema programiranje rotorjev, plugboarda in zrcala
+# Drugi del zajema združitev različnih možnosti teh nastavitev v razred "Koda", ki nam omogoča kodiranje
+# Tretji del zajema funkcije, ki potem kodirajo posamezno črko in besedilo
 
-    for znak in delno_urejen1:
-        if znak in ang_abc or znak == ' ':
-            delno_urejen2 += znak
-
-    urejen_tekst = ' '.join(delno_urejen2.split())
-
-    return urejen_tekst
-
-
-testno_besedilo = 'Jaz pišem besedilo s katerim bom \t preveril kaj dogaja 123:::___// SDŠŠĆĆŽŽČčč \n \n \t'
+#_____________________________________________________________________________________________________________
+#_____________________________________________________________________________________________________________
+# 1. del:
 
 # Za kodiranje naša enigma potrebuje 3 rotorje, v vsakemu od katerega se črka spremeni v drugo črko
 # te rotorje bomo oblikovali kot razred
-# rotor je vresnici permutacija 26 črk angleške abecede brez negibnih točk
+# Rotor je vresnici permutacija 26 črk angleške abecede brez negibnih točk z možnim zamikom (če se rotor obrne)
+# črka bo potovala skozi te tri rotorje, se v vsakemu spremenila, se spremenila v zrcalu in šla nazaj skozi rotorje
 # Posamezne črke bomo ponazorili z njihovo zaporedno številko v abecedi. a je 0, b je 1, c 2 itd.
-# sestavimo funkcijo, ki preveri, če je dan seznam prvih 26 številk res ustrezna permutacija
-# identiteta je, kot bi ime namigovalo, le urejen seznam številk 1 do 26
+# Sestavimo najprej funkcijo, ki preveri, če je dan seznam prvih 26 številk res ustrezna permutacija
+# Identiteta je, kot bi ime namigovalo, le urejen seznam številk 1 do 26
 
 identiteta = list(range(26))
 
@@ -44,7 +36,7 @@ def preveri_rotor(per):
     return True
 
 
-# še funkcija, ki nam sestavi naključno permutacijo za rotor (potrebno import random)
+# Sedaj potrebujemo funkcijo, ki nam sestavi naključno permutacijo za rotor
 def ustvari_rotor():
     rotor = random.sample(identiteta, len(identiteta))
     if preveri_rotor(rotor):
@@ -52,16 +44,16 @@ def ustvari_rotor():
     else:
         return ustvari_rotor()
     
-# v fizični obliki enigme so imeli dostop do petih rotorjev, iz katerih so izbrali 3 za posamezno kodiranje
-# ker nimam dostopa do informacije, kakše točne permutacije so bili te rotorji, jih bomo naključno generirali
-# zapišimo jih kot konstante
+# V fizični obliki enigme so imeli dostop do petih rotorjev, iz katerih so izbrali 3 za posamezno kodiranje
+# Ker nimam dostopa do informacije, kakše točne permutacije so bili te rotorji, jih bomo naključno generirali
+# Zapišimo jih kot konstante
 per1 = [9, 13, 7, 18, 25, 20, 3, 4, 2, 22, 23, 6, 1, 24, 16, 11, 5, 15, 12, 0, 19, 8, 14, 10, 17, 21]
 per2 = [5, 17, 9, 7, 6, 18, 23, 0, 12, 13, 14, 10, 25, 24, 20, 8, 1, 3, 22, 11, 21, 19, 2, 15, 16, 4]
 per3 = [25, 11, 15, 21, 0, 10, 20, 3, 24, 12, 13, 5, 1, 19, 23, 7, 18, 9, 22, 4, 16, 8, 6, 2, 14, 17]
 per4 = [14, 20, 18, 25, 3, 17, 10, 13, 15, 24, 4, 6, 22, 1, 12, 23, 9, 21, 7, 5, 11, 8, 16, 2, 0, 19]
 per5 = [13, 12, 18, 16, 7, 21, 5, 22, 19, 25, 24, 23, 4, 0, 3, 17, 14, 8, 6, 15, 1, 10, 2, 11, 20, 9]
 
-# definirajmo sedaj razred Rotor skupaj z nekaj osnovnimi funkcijami
+# Definirajmo sedaj razred Rotor skupaj z nekaj osnovnimi funkcijami
 class Rotor:
 
     def __init__(self, per, pozicija):
@@ -74,23 +66,25 @@ class Rotor:
     def __rept__(self):
         return 'Rotor({0}, {1})'.format(self.per, self.poz)
 
+# Po tem, ko enigma kodira posamezno črko, se rotorji obrnejo, tako da za naslednjo črko koda ne ostane ista
+# Ta obrat bomo simulirali z zamikom, ki smo ga definirali preko argumenta pozicija
+
     def rotiraj(self):
         return Rotor(self.per, (self.poz + 1) % 26)
 
+# Potrebujemo še 2 funkciji, ki permutirata dano črko (številko) glede na lastnosti našega rotorja
+# prva funkcija ustreza permutaciji, ko gre črko skozi rotor, druga pa obratni smeri
 def kodiraj_rotor(x, rotor):
     return rotor.per[(x + rotor.poz) % 26]
 
 def kodiraj_rotor_inverz(x, rotor):
     inverz_permutacije = rotor.per.index(x)
     return (inverz_permutacije - rotor.poz) % 26
-
-testni_rotor = Rotor(per3, 24)
     
-
-# ko se črke trikrat permutira skozi rotorje, se zamenja v "zrcalu" in nato ponovno gre skozi rotorje
-# zrcalo je torej permutacija 26 črk z 13 različnimi transpozicijami
-# kot za rotor, ustvarimo funkcijo, ki preveri če je zrcalo res to
-# za razliko od rotorjev, ki se, kot bi ime namigovalo, po vsaki črki obrnejo, je zrcalo konstantno
+# Ko se črke trikrat permutira v rotorjih, se zamenja v "zrcalu" in nato ponovno gre skozi rotorje
+# Zrcalo je torej permutacija 26 črk z 13 različnimi transpozicijami
+# Kot za rotor, ustvarimo funkcijo, ki preveri če permutacija primerna
+# Za razliko od rotorjev, ki se, kot bi ime namigovalo, po vsaki črki obrnejo, je zrcalo konstantno
 
 def preveri_zrcalo(per):
     if not preveri_rotor(per):
@@ -100,8 +94,9 @@ def preveri_zrcalo(per):
             return False
     return True
 
-# in še funkcija ki ustvari naključno zrcalo.
+# In še funkcija ki ustvari naključno zrcalo.
 # Tu bo treba biti rahlo bolj zvit, saj z povsem naključno generacijo ponavadi ne dobimo ustrezne permutacije
+
 def ustvari_zrcalo():
     zrc = [0] * 26
     preostale_stevilke = identiteta.copy()
@@ -115,16 +110,18 @@ def ustvari_zrcalo():
 
     return zrc
 
-# Konstantno zrcalo, ki ga bomo uporabljali je
+# Konstantno zrcalo, ki ga bomo uporabljali je (vzel sem enega naključno generiranega)
 zrcalo = [8, 14, 25, 19, 10, 22, 9, 18, 0, 6, 4, 13, 16, 11, 1, 17, 12, 15, 7, 3, 23, 24, 5, 20, 21, 2]
-# definirajmo funkcijo, kaj se zgodi s številko, ko gre skozi zrcalo:
+
+# Za konec potrebujemo še funkcijo, ki permutira črko ko ta pride do zrcala
 def kodiraj_zrcalo(x, zrc = zrcalo):
     return zrc[x]
 
 
 # Zadnja stvar, ki jo potrebujemo za uspešno kodiranje, je plugboard
-# plugboard omogoča nastavitve, da povežemo 2 črki, ki se potem zamenjate preden in/ali po tem, ko črka pride iz rotorjev
-# plugboard je torej sestavljen iz 1 do 13 permutacij črk
+# Plugboard omogoča nastavitve, da povežemo 2 črki, ki se potem zamenjate preden in/ali po tem, ko črka pride iz rotorjev
+# Plugboard je torej sestavljen iz 1 do 13 transpozicij črk, na ostalih črkah pa je identiteta
+# Ponovno sestavimo funkcijo, ki preveri, če je permutacija ustrezna
 
 def preveri_plugboard(per):
     for i in range(26):
@@ -136,8 +133,8 @@ def preveri_plugboard(per):
     return True
 
 
-# definiramo funkcijo, ki nam ustvari naključen plugboard.
-# verjetnost, da se 2 naključno izbrani številki povežeta je arbitrarno določena na 2/3
+# Definiramo funkcijo, ki nam ustvari naključen plugboard.
+# Verjetnost, da se 2 naključno izbrani številki povežeta je arbitrarno določena na 2/3. Lahko bi bila karkoli
 
 def ustvari_plugboard():
     pb = [0] * 26
@@ -159,11 +156,19 @@ def ustvari_plugboard():
 
     return pb
 
+# Za testne primere bomo uporabljali arbitrarno določene plugboard nastavitve
 plugboard = [0, 23, 3, 2, 4, 17, 6, 11, 8, 13, 10, 7, 22, 9, 16, 19, 14, 5, 18, 15, 20, 21, 12, 1, 24, 25]
 
+# Za konec seveda še funkcija, ki permutira črko, ko ta gre skozi plugboard
 def kodiraj_plugboard(x, pb = plugboard):
     return pb[x]
 
+
+#_____________________________________________________________________________________________________________
+#_____________________________________________________________________________________________________________
+# 2. del:
+
+# Definirajmo razred "Koda", ki vsebuje 3 rotorje v določenem vrstnem redu, plugboard in zrcalo
 
 class Koda:
 
@@ -178,8 +183,12 @@ class Koda:
         x1 = self.rot1
         x2 = self.rot2
         x3 = self.rot3
-        return 'Koda:\n\trotorji: {0}, {1}, {2}\n\tplugboard: {3}\n\tzrcalo:{4}'.format(x1, x2, x3, self.pb, self.zrc)
-
+        return 'Koda:\nrotorji: {0}, {1}, {2}\nplugboard: {3}\nzrcalo:{4}'.format(x1, x2, x3, self.pb, self.zrc)
+# Kot sem že prej omenil, se po vsaki črki rotorji obrnejo, tako da se koda spremeni
+# Obrnejo se po naslednjem pravilu:
+#   - 1. rotor se vedno obrne
+#   - 2. rotor se obrne na vsakih 26 obratov prvega. Torej le ko gre pozicija 1. iz 25 na 0
+#   - 3. rotor se obrne na vsakih 26 obratov drugega
     def spremeni(self):
         if self.rot1.poz == 25:
             if self.rot2.poz == 25:
@@ -187,9 +196,31 @@ class Koda:
             return Koda(self.rot1.rotiraj(), self.rot2.rotiraj(), self.rot3, self.pb, self.zrc)
         return Koda(self.rot1.rotiraj(), self.rot2, self.rot3, self.pb, self.zrc)
 
+#_____________________________________________________________________________________________________________
+#_____________________________________________________________________________________________________________
+# 3. del:
 
+# Sedaj lahko sestavimo fukncije, ki nam kodirajo besedilo
+# Najprej sestavimo funkcijo, ki besedilo spremeni v sprejemljivo obliko
+# To pomeni da odstrani vse znake, ki niso v angleški abecedi in spremeni vse črke v male
+# Presledke pusti pri miru, če nastane več zaporednih presledkov jih spremeni v enega
+# Šumnike spremeni v črke brez strešice. Ostale črke (npr ć, đ) izbriše
+# Za angleško abecedo importamo string
 
-# Funkciji ki preko seznama črk iz abecede spremenita črko v njeno zaporedno št v abecedi in obratno
+def uredi_besedilo(tekst):
+    delno_urejen1 = tekst.lower().replace('č', 'c').replace('š', 's').replace('ž', 'z')
+    delno_urejen2 = ''
+    ang_abc = list(string.ascii_lowercase)
+
+    for znak in delno_urejen1:
+        if znak in ang_abc or znak == ' ':
+            delno_urejen2 += znak
+
+    urejen_tekst = ' '.join(delno_urejen2.split())
+
+    return urejen_tekst
+
+# Funkciji, ki preko seznama črk iz abecede spremenita črko v njeno zaporedno št v abecedi in obratno
 def crka_v_stevilko(crka):
     stevilka = list(string.ascii_lowercase).index(crka)
     return stevilka
@@ -198,10 +229,14 @@ def stevilka_v_crko(stevilka):
     crka = list(string.ascii_lowercase)[stevilka]
     return crka
 
+# Funkcija, ki kodira posamezno črko glede na nastavitev kode
+# Funkcija je vresnici zmnožek permutacij črk: na koncu dobimo transpozicije
+# Funkcija je torej sam svoj inver: če iz kodirane črke želimo originalno, jo le vstavimo pod istimi pogoji
+
 def kodiraj_crko(crka, koda):
     # Črko najprej spremenimo v številko
     x0 = crka_v_stevilko(crka)
-    # Potem številko pošlemo skozi plugboard
+    # Potem številko pošlemo skozi plugboard (morda se spremeni morda pa ne)
     x1 = kodiraj_plugboard(x0, koda.pb)
     # Nato skozi vse 3 rotorje
     x2 = kodiraj_rotor(x1, koda.rot1)
@@ -213,16 +248,18 @@ def kodiraj_crko(crka, koda):
     x6 = kodiraj_rotor_inverz(x5, koda.rot3)
     x7 = kodiraj_rotor_inverz(x6, koda.rot2)
     x8 = kodiraj_rotor_inverz(x7, koda.rot1)
-    # In še enkrat skozi plugboard
+    # In še enkrat skozi plugboard (spet, morda se spremeni morda ne)
     x9 = kodiraj_plugboard(x8, koda.pb)
-    # Številka se tako spremeni 7-9 krat, odvisno od plugboard nastavitev
+    # Številka se tako spremeni 7 do 9 krat, odvisno od plugboard nastavitev
     # Sedaj številko spremenimo nazaj v črko
     return stevilka_v_crko(x9)
 
-testna_koda1 = Koda(Rotor(per1, 25), Rotor(per2, 77), Rotor(per5, 4), plugboard, zrcalo)
-testna_koda2 = Koda(Rotor(per1, 25), Rotor(per2, 77), Rotor(per5, 8), plugboard, zrcalo)
+# Za konec še funkcija, ki kodira celotno besedilo
+# Funkcija vzame besedilo in ga uredi po pravilih opisanih zgoraj
+# Nato gre čez vse znake urejenega besedila:
+# Če je znak presledek, ga pusti pri miru
+# Če pa je znak črka, jo kodira preko funkcije kodiraj_crko, nato pa ustrezno spremeni kodo
 
-print(uredi_besedilo(testno_besedilo))
 
 def kodiraj_besedilo(besedilo, koda):
     urejeno_besedilo = uredi_besedilo(besedilo)
@@ -235,7 +272,6 @@ def kodiraj_besedilo(besedilo, koda):
             koda = koda.spremeni()
     return kodirano_besedilo
 
-kodiran_test = kodiraj_besedilo(testno_besedilo, testna_koda1)
-print(kodiran_test)
-
-print(kodiraj_besedilo(kodiran_test, testna_koda1))
+# Za dekodiranje besedila uporabimo isto funkcijo
+# Kot opisano prej v opisu funkcije kodiraj_crko, je funkcija sam svoj inverz
+# Torej moramo le vstaviti kodirano besedilo pod istimi začetnimi pogoji (Kodo)
